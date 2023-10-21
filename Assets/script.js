ba8704f4063e2e402a4d3c21ee2cb23b1a4491a1
L.lar <l.lar1109@gmail.com>
2023-10-21 18:38:44 -0400

    everything is functioning but the answers need more work

---
+++ Assets/script.js
@@ -0,0 +1,88 @@
+const questions = [
+  {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+      correctAnswer: "Alerts"
+  },
+  {
+      question: "The condition in an if/else statement is enclosed within ___.",
+      choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+      correctAnswer: "Parenthesis"
+  },
+  {
+        question: "Arrays in JavaScript can be used to store ___.",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+        correctAnswer: "All of the above"
+  },
+  {
+        question: "String values must be enclosed within ___ when being assigned to variables.",
+        choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        correctAnswer: "Quotes"
+  },
+  {
+        question: "A very useful tools used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "Terminal/Bash", "For loops", "console.log"],
+        correctAnswer: "console.log"
+  },
+];
+
+const quizEl = document.querySelector(".quiz")
+const questEl = document.getElementById("questions");
+const choices = document.getElementById("choices");
+const startBtn = document.getElementById("begin");
+const results = document.getElementById("result");
+var nameEl = document.getElementById("name");
+
+let currentQuestion = 0;
+let score = 0;
+
+startBtn.addEventListener("click", function() {
+      nameEl.style.display = "none";
+      questEl.style.display = "block";
+      choices.style.display = "flex";
+      displayQuestion(currentQuestion);
+
+  });
+  function displayQuestion(questionIndex) {
+      if (questionIndex < questions.length) {
+        const questionData = questions[questionIndex];
+        questEl.innerHTML = `<p>${questionData.question}</p>`;
+        choicesContainer.innerHTML = "";
+    
+        questionData.choices.forEach((choice, index) => {
+          const choiceElement = document.createElement("label");
+          choiceElement.innerHTML = `
+            <input type="radio" name="answer" value="${choice}">
+            ${choice}
+          `;
+          choiceElement.addEventListener("click", function() {
+            checkAnswer(choice, questionData.correctAnswer);
+          });
+          choicesContainer.appendChild(choiceElement);
+        });
+      } else {
+        // Quiz is complete, display the result
+        displayResult();
+      }
+    }
+    
+    function checkAnswer(selectedChoice, correctAnswer) {
+      if (selectedChoice === correctAnswer) {
+        score++;
+      }
+    
+      currentQuestion++;
+      displayQuestion(currentQuestion);
+    }
+    
+    function displayResult() {
+      questEl.style.display = "none";
+      results.style.display = "block";
+      results.innerHTML = `You scored ${score} out of ${questions.length} questions.`;
+    }
+    
+
+  
+  
+  
+  console.log(startBtn)
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# WebApiQuiz
+# Web API Quiz
+
+    Our task for this weeks Challenge is to create a website from scratch that provides a timed pop quiz on web development. The website also keeps record of our previous scores.
+
+
+## Acceptance Criteria
+* It is done when index.HTML is created along with CSS stylesheet
+* It is done when all assets is connected with the index.HTML
+* It is done when the page provide a link that keeps record of your previous score
+* It is done when the quiz is time once you start your quiz
+* It is done when the quiz provide true and false when you select an answer
+
+
+
